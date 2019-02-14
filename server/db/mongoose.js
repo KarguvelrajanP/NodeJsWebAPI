9a9e0d5924776a70be47412c8f64234fa4d03d9f
Karguvelrajan <Karguvelrajan.P@gmail.com>
2019-02-14 19:14:55 +0530

    mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Admin:yRWcbllDeAISZcpb@cluster0-xpgzz.azure.mongodb.net/EmployeeDB?retryWrites=true' || 'mongodb://localhost:27017/APIDb', { useNewUrlParser: true });
-
+mongoose.connect('mongodb+srv://Admin:yRWcbllDeAISZcpb@cluster0-xpgzz.azure.mongodb.net/EmployeeDB?retryWrites=true', { useNewUrlParser: true });
+// || 'mongodb://localhost:27017/APIDb'|| 
 module.exports = { mongoose }; 
