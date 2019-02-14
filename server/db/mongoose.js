05640154533639f294b9d525e5cb4c42796d0d23
Karguvelrajan <Karguvelrajan.P@gmail.com>
2019-02-14 19:11:57 +0530

    Changed mongoose connect code in mongoose.js file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/APIDb'|| 'mongodb+srv://Admin:yRWcbllDeAISZcpb@cluster0-xpgzz.azure.mongodb.net/EmployeeDB?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://Admin:yRWcbllDeAISZcpb@cluster0-xpgzz.azure.mongodb.net/EmployeeDB?retryWrites=true' || 'mongodb://localhost:27017/APIDb', { useNewUrlParser: true });
 
 module.exports = { mongoose }; 
