3fce050b832e6da0a2e45b977705704c02b0e80b
Karguvelrajan <Karguvelrajan.P@gmail.com>
2019-02-15 12:51:03 +0530

    Modified the code to connect the mongodb to access globally

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Admin:yRWcbllDeAISZcpb@cluster0-xpgzz.azure.mongodb.net/EmployeeDB?retryWrites=true', { useNewUrlParser: true });
-// || 'mongodb://localhost:27017/APIDb'|| 
+mongoose.connect('mongodb+srv://Admin:btlL2eI5dONpX7hj@cluster0-dzrou.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 module.exports = { mongoose }; 
