f32931c55bf6367a4d376daa94d4c1f1ef5de8f1
Karguvelrajan <Karguvelrajan.P@gmail.com>
2019-02-15 14:25:51 +0530

    Modified the mongoose js file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/APIDb' || 'mongodb+srv://Admin:btlL2eI5dONpX7hj@cluster0-dzrou.mongodb.net/employeeDB?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://Admin:btlL2eI5dONpX7hj@cluster0-dzrou.mongodb.net/employeeDB?retryWrites=true', { useNewUrlParser: true });
+
+// 'mongodb://localhost:27017/APIDb' || 
 module.exports = { mongoose }; 
